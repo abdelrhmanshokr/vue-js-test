7e9dee79dd848840e090dfbc3d2b6579074c030f
abdelrhmanshokr <abdelrhmanshokr13@gmail.com>
2020-04-15 12:33:14 +0200

    added some new events click and double click

--- app.js
+++ app.js
@@ -5,19 +5,37 @@ new Vue({
     //the second proprety is the data which tells the instanse what to output 
     data: {
         name: 'name',
-        age: 'number',
-        address: 'address',
+        
         //this link is used in the href attribute in a link so we need to bind it to take its value in the html page as shown in the html page 
         link: 'https://www.google.com',
         inputValue: 'text',
+        
         //we also can write html in here and bind it in the html page link so 
-        linkTag: '<a href="https://www.google.com"> google.com </a>'
+        linkTag: '<a href="https://www.google.com"> google.com </a>',
+
+        //events data 
+        age: 22,
+        x: 0,
+        y: 0
     },
     //we can add methods to any element also 
     //the arrow function does not support this notation
     methods: {
         greet: function(name){
            return 'hello' + ' ' + name + ' ' + this.name;
+        },
+
+        //events methods 
+        addAge: function(inc){
+            this.age += inc;
+        },
+        subAge: function(dec){
+            this.age -= dec;
+        },
+
+        updateXY: function(event){ //when we create a new event we get an event object automatically called event
+            this.x = event.offsetX;
+            this.y = event.offsetY;
         }
     }
 });
