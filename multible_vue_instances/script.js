c6b7cafa965cc1de7f0fec6291737938565e3444
abdelrhmanshokr <abdelrhmanshokr13@gmail.com>
2020-04-27 17:12:51 +0200

    added a new multible vue object file with a simple component in it

---
+++ multible_vue_instances/script.js
@@ -0,0 +1,58 @@
+//a component is a reusable piece of code
+//to create it we use Vue.component('component name', {object of parameters});
+Vue.component('greetings', {
+    template: '<p>hi from the greetings component, this is {{name}}. <button v-on:click="changeName">change name</button></p>',
+
+    //we can pass data to this component as well but it's not an object directly it must be a function then that function will return a data object  
+    data: function(){
+        return {
+            name: 'shokr'
+        }
+    },
+
+    //we can pass methods just like we use them in a vue object 
+    methods: {
+        changeName: function(){
+            this.name = 'abdelrhman';
+        }
+    }
+});
+
+let one = new Vue({
+    el:'#vue-app-one',
+
+    data:{
+        title: 'vue app one'
+    },
+
+    mehtods:{
+
+    },
+
+    computed:{
+        greet: function(){
+            return 'hello from app one';
+        }
+    }
+});
+
+
+let two = new Vue({
+    el:'#vue-app-two',
+
+    data:{
+        title: 'vue app two'
+    },
+
+    methods:{
+        changeAppOnTitle: function(){
+            one.title = 'title one changed';
+        }
+    },
+
+    computed:{
+        greet: function(){
+            return 'hello from app two';
+        }
+    }
+})
